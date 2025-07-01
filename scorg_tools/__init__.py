7416e1b88f66edd7edb3a6265b27345fec352da8
Star-Destroyer <Star-Destroyer@scorg.tools>
2025-07-01 22:25:03 +0100

    Version bump

--- scorg_tools/__init__.py
+++ scorg_tools/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "SCOrg.tools Blender Tools beta",
     "author": "Star-Destroyer@scorg.tools",
-    "version": (1, 1, 37),
+    "version": (1, 1, 38),
     "blender": (3, 6, 0),
     "location": "View3D > Sidebar > SCOrg.tools",
     "description": "Tools to supplement StarFab",
